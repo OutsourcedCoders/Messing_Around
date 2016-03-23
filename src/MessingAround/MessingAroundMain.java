2b1e031b80bfa864a5aaa77c7e6de447775e4bd9
jorge_flore141 <jorge_flore141@ANDHS-E111LB-30.ah.isd11>
2016-03-23 15:20:07 -0500

    first commit

---
+++ src/MessingAround/MessingAroundMain.java
@@ -0,0 +1,14 @@
+package MessingAround;
+
+public class MessingAroundMain {
+     //Scanner sc = new Scanner(System.in);
+     //int i = sc.nextInt();
+    public static void main(String[] args) {
+        
+        String what = "man";
+        System.out.println("Hey, how are yah?");
+        
+        System.out.println();
+        System.out.println("");
+    }
+}
