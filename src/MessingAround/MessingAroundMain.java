ddec34caad85b8af5c21b54e5944dee5c002ae19
andy_zheng422 <andy_zheng422@ANDHS-E111LB-28.ah.isd11>
2016-03-23 15:53:55 -0500

    Greeting

--- src/MessingAround/MessingAroundMain.java
+++ src/MessingAround/MessingAroundMain.java
@@ -1,14 +1,35 @@
 package MessingAround;
+import java.util.Scanner;
 
-public class MessingAroundMain {
-     //Scanner sc = new Scanner(System.in);
-     //int i = sc.nextInt();
+public class MessingAroundMain{
+    public static Scanner sc = new Scanner(System.in);
+    public static String userInput = "User";
+    public static boolean userAnswered = false;
     public static void main(String[] args) {
         
-        String what = "man";
-        System.out.println("Hey, how are yah?");
+        while(userAnswered == false) {
+            
+        System.out.print("Hey, how are yah?: ");
+        userInput = sc.next();
         
-        System.out.println();
-        System.out.println("");
+        switch (userInput.toUpperCase()) {
+            case "GOOD":
+                System.out.println("That's good to hear");
+                userAnswered = true;
+                break;
+            case "BAD":
+                System.out.println("That's just too bad");
+                userAnswered = true;
+                break;
+            case "SO SO":
+                System.out.println("That's okay");
+                userAnswered = true;
+                break;
+            default: 
+                System.out.println("Please input a valid response");
+                userInput = sc.next();
+                break;
+            }
+        }
     }
 }
