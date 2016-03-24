7103e3239453aad963a0e42a61c20e36709809e2
Jasonface900 <jorge.floresm900@gmail.com>
2016-03-24 16:18:14 -0500

    added game

---
+++ src/RPGMonsterFighter/RPG_Game.java
@@ -0,0 +1,8 @@
+package RPGMonsterFighter;
+import MessingAround.UserInputs;
+import RPGMonsterFighter.CreaturesAndAttributes.Zombie;
+import RPGMonsterFighter.CreaturesAndAttributes.Attributes;
+
+public class RPG_Game extends UserInputs{
+    
+}
