3b4bb72b6e686b7cc3cc7025cdd6d5c5e797399b
jorge_flore141 <jorge_flore141@ANDHS-B202LB-07.ah.isd11>
2016-03-24 09:33:34 -0500

    renamed stuff

--- src/MessingAround/UserInputs.java
+++ src/MessingAround/UserInputs.java
@@ -2,7 +2,7 @@ package MessingAround;
 
 import java.util.Scanner;
 
-public class userInput {
+public class UserInputs {
         public static Scanner sc = new Scanner(System.in);
     public static String userInput = "User";
     public static boolean userAnswered = false;
