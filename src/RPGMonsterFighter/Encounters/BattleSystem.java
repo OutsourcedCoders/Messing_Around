ceced561f9f0f64c470135e3cb851467472405f3
Jasonface900 <jorge.floresm900@gmail.com>
2016-03-30 10:38:50 -0500

    cleaned up BattleSystem.java

--- src/RPGMonsterFighter/Encounters/BattleSystem.java
+++ src/RPGMonsterFighter/Encounters/BattleSystem.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package RPGMonsterFighter.Encounters;
 
-/**
- *
- * @author jorge_flore141
- */
-public class BattleSystem {
-    
+public class BattleSystem extends MonsterChoice{
+    public static void main(String[] args) {
+        while(inBattle = true){
+            
+        }
+    }
 }
