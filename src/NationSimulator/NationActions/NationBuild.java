6bd616bc884c97357e59b15db44a8ad14f3a58cd
YourAverageRandy <andy.zheng.v3@gmail.com>
2016-03-31 09:56:53 -0500

    added 2 actions under build

--- src/NationSimulator/NationActions/NationBuild.java
+++ src/NationSimulator/NationActions/NationBuild.java
@@ -4,16 +4,27 @@ import MessingAround.UserInputs;
 
 public class NationBuild extends UserInputs{
     public static void main(String[] args) {
-        while(!userAnswered) {
-            System.out.println("-----------------------------------------------"
-                    + "--------------------------");
-            System.out.println("|  Build more homes  |  Build more travel roads"
-                    + "  |  Build more weapons  |");
-            System.out.println("-----------------------------------------------"
-                    + "--------------------------");
+        while(turnSubActive) {
+            System.out.println("-------------------------------------------------------------------------");
+            System.out.println("|  Build more homes  |  Build more travel roads  |  Build more weapons  |");
+            System.out.println("-------------------------------------------------------------------------");
             userInput = sc.nextLine();
-            switch(userInput) {
-                
+            userAnswered = false;
+            while(!userAnswered) {
+                switch(userInput.toUpperCase()) {
+                    case "BUILD MORE HOMES": 
+                        System.out.println("Each house increases population limit by " + houseTech 
+                                + " at a cost of " + houseCost + " gold");
+                        System.out.print("Input number of homes to be built:");
+                        houseBuild = sc.nextInt();
+                        if(houseBuild * houseCost <= nationWealth) {
+                            houseCount = houseCount + houseBuild; 
+                            userAnswered = true;
+                        }
+                        else{
+                            System.out.println("Insufficient Gold");
+                        }
+                }
             }
         }
     }
--- src/NationSimulator/NationTurn/TurnEnd.java
+++ src/NationSimulator/NationTurn/TurnEnd.java
@@ -5,7 +5,11 @@ import MessingAround.UserInputs;
 public class TurnEnd extends UserInputs{
     public static void main(String args[]) {
         NationSimulator.NationEvents.main(args);
+        populationLimit = houseTech * houseCount;
         nationPopulation = (int) (nationPopulation * (nationGrowthRate)); 
+        if (nationPopulation > populationLimit) {
+            nationPopulation = populationLimit;
+        }
         nationStrength   = nationPopulation * individualStrength; 
         nationWealth     = nationWealth + nationWealthInc; 
         turnCount        = turnCount + 1;
