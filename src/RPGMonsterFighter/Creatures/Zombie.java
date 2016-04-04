8d7b839bc1077cc547f6199fbff7d51689e5f278
Jasonface900 <jorge.floresm900@gmail.com>
2016-04-04 22:24:40 +0930

    added a ton of comments

--- src/RPGMonsterFighter/Creatures/Zombie.java
+++ src/RPGMonsterFighter/Creatures/Zombie.java
@@ -7,19 +7,21 @@ public class Zombie extends RPG_Game{
         if (justEncountered){
             System.out.println("\nYou see a horrifying ZOMBIE.");
             DefaultCreature Zombie = new DefaultCreature(100, 0, 0);
-            Zombie.setHealth(100 + NumGen.nextInt(25));
+            Zombie.setHealth(100 + NumGen.nextInt(25));//                        Default health 
         }
         if (attacking){
-            int damageDone = NumGen.nextInt(damageRandom);
-            damageDone = damageDone * weaponClass;
-            //Zombie.loseHealth(damageDone);
+            int damageDone = NumGen.nextInt(damageRandom);//                     Randomizes damage from a val of 0-5
+            damageDone = damageDone * weaponClass;//                             Currently unused
+            damageDone = damageDone + 1;//                                       Adds 1 to damage done to do at least 1 damage
+            //Zombie.loseHealth(damageDone);                         //          Script for doing damage
             System.out.println("You did " + damageDone + " damage.");
         }else if (!attacking){
             int damageTaken = NumGen.nextInt(damageRandom);
             damageTaken = damageTaken - plrDef;
-            Player.loseHealth(damageTaken);
-            System.out.println("Oh no!");
-            System.out.println("You took " + damageTaken + " damage!");   
+            damageTaken = damageTaken + 1;//                                     Adds 1 to damage taken to take at least 1 damage
+            Player.loseHealth(damageTaken);//                                    Script for taking damage
+            System.out.println("\nOh no!");
+            System.out.println("You took " + damageTaken + " damage!");
         }if (plrHp == 0){
             alive = false;
         }
